affb7319335a21a8a6b6dd3f2d34e34870f77623
Nandhagopiv <nandhagopy@gmail.com>
2024-08-16 02:48:46 +0530

    second commit to updated json file...............

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/oauth2callback', async (req, res) => {
     const { tokens } = await oAuth2Client.getToken(code);
     oAuth2Client.setCredentials(tokens);
     fs.writeFileSync('tokens.json', JSON.stringify(tokens));
-    res.redirect('http://localhost:3000/emails')
+    res.redirect('https://task-ruby-three.vercel.app/oauth2callback/emails')
   } catch (error) {
     console.error('Error retrieving access token', error);
     res.status(500).send('Error during authentication');
